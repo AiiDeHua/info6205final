df311c16a8100dd78a89392ca7e7dac6b6094c4a
edwardxie <edwardde@udel.edu>
2021-12-15 15:44:48 -0500

    fix pell final

--- src/edu/neu/Pell.java
+++ src/edu/neu/Pell.java
@@ -26,22 +26,4 @@ public class Pell {
         return p0 + 2 * p1;
     }
 
-    public static void main(String[] args) {
-        Pell a = new Pell();
-        System.out.println(a.get(0));
-        System.out.println(a.get(1));
-        System.out.println(a.get(2));
-        System.out.println(a.get(3));
-    }
-//
-//    P0 = 0
-//
-//    P1 = 1
-//
-//    Pn = 2 Pn-1 + Pn-2
-//    p2 = 2*p1+p0 2
-//    p3 = 2*p2+p1 5
-
-//    int first = 0; int second = 1; int nth = 1; for (int i = 2; i <= n; i++) { nth = first + second; first = second; second = nth; }
-
 }
