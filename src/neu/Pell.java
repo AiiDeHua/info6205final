2c996da34dfce87611d3abefe21f17ba4d7c2737
edwardxie <edwardde@udel.edu>
2021-12-15 15:41:55 -0500

    fix pell

--- src/neu/Pell.java
+++ src/neu/Pell.java
@@ -13,15 +13,17 @@ public class Pell {
         }else if(n == p1){
             return p1;
         }
-//        long result = 1;
-//        long tmp = 1;
-//        for(int i= 1;i<n;i++){
-//            result += p0 + 2 * p1;
-//            p0 = p1;
-//            p1 = result;
-//        }
-        return 2*get(n-1) + get(n-2);
-//        return result;
+        long result = p0 + 2 * p1;
+        for(int i= 2;i<n;i++){
+            p0 = p1;
+            p1 = result;
+            result = get2(p1,p0);
+        }
+        return result;
+    }
+
+    private long get2(long p1,long p0){
+        return p0 + 2 * p1;
     }
 
     public static void main(String[] args) {
